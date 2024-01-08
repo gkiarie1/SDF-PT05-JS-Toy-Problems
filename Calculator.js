6ae8d230d649937cd8edfbf855d761225087c63a
gkiarie1 <gkaris24@gmail.com>
2024-01-08 18:56:46 +0300

    Updates

--- Calculator.js
+++ Calculator.js
@@ -1,25 +1,19 @@
 function calculateGrade() {
-    var html = parseInt(document.getElementById('html').value) || 0;
-    var css = parseInt(document.getElementById('css').value) || 0;
-    var javascript = parseInt(document.getElementById('javascript').value) || 0;
-  
-    var finalScore = html + css + javascript;
-    displayGrade(html, css, javascript, finalScore);
+    var score = parseInt(document.getElementById('score').value) || 0;
+    displayGrade(score);
 }
-  function displayGrade(html, css, javascript,finalScore) {
-    var message = `HTML: ${html}\nCSS: ${css}\nJavaScript: ${javascript}\nTotal Score: ${finalScore}\n\n`;
-    if (finalScore>90)
-    alert(message + "Must be nice! You showoff");
-    else if (finalScore >= 79) {
+  function displayGrade(score) {
+    var message = `Score: ${score}\n`;
+    if (score >= 79) {
       alert(message + "Congratulations! Keep up the good work. You got an A");
-    } else if (60 <= finalScore && finalScore <= 78) {
+    } else if (60 <= score && finalScore <= 78) {
       alert(message + "Hooray! You have a B");
-    } else if (49 <= finalScore && finalScore <= 59) {
+    } else if (49 <= score && finalScore <= 59) {
       alert(message + "Pull up your socks! You have a C");
-    } else if (40 <= finalScore && finalScore <= 48) {
+    } else if (40 <= score && finalScore <= 48) {
       alert(message + "Work Harder! You have a D");
     } else if (
-        39<=finalScore) {
+        39<=score) {
       alert(message + "Oops! You Failed. You have an E");
     }
 }
