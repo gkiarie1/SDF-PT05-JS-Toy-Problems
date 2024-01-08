1d005306147e4444a6930d973a949c0d37644d29
gkiarie1 <gkaris24@gmail.com>
2024-01-08 19:44:18 +0300

    Updates

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,13 @@
 function displayGrade() {
     var score = parseInt(document.getElementById('score').value) || 0;
     var message = `Score: ${score}\n`;
-    if (score >= 79) {
+    if (score >79) {
       alert(message + "Congratulations! Keep up the good work. You got an A");
-    } else if (60 <= score && finalScore <= 78) {
+    } else if (60 <= score && score <= 78) {
       alert(message + "Hooray! You have a B");
-    } else if (49 <= score && finalScore <= 59) {
+    } else if (49 <= score && score <= 59) {
       alert(message + "Pull up your socks! You have a C");
-    } else if (40 <= score && finalScore <= 48) {
+    } else if (40 <= score && score <= 48) {
       alert(message + "Work Harder! You have a D");
     } else if (
         39<=score) {
